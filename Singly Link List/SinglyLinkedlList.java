3e6d91319b49cf46794c95712f161d0ef7971b61
Faizee095 <faiyazsiddique9@gmail.com>
2022-03-08 19:30:32 +0530

    Insert Node at start and end

--- Singly Link List/SinglyLinkedlList.java
+++ Singly Link List/SinglyLinkedlList.java
@@ -32,23 +32,45 @@ public class SinglyLinkedlList{
         return count;
     }
 
+    public void insertFirst(int value){
+        ListNode n1=new ListNode(value);
+        n1.next=head;
+        head=n1;
+    }
+
+    public void insertLast(int value){
+        ListNode n1=new ListNode(value);
+        if(head==null){
+            head=n1;
+            return;
+        }
+
+        ListNode current=head;
+        while(current.next!=null){
+            current=current.next;
+        }
+        current.next=n1;
+    }
+
+
 
     public static void main(String[] args) {
         SinglyLinkedlList sll=new SinglyLinkedlList();
         sll.head=new ListNode(10);
-        ListNode Second=new ListNode(20);
-        ListNode Third=new ListNode(30);
-        ListNode Fourth=new ListNode(40);
+         ListNode Second=new ListNode(20);
+        // ListNode Third=new ListNode(30);
+        // ListNode Fourth=new ListNode(40);
 
 
-        sll.head.next=Second;
-        Second.next=Third;
-        Third.next=Fourth;
+         sll.head.next=Second;
+        // Second.next=Third;
+        // Third.next=Fourth;
 
 
 
-        // sll.display();
-       System.out.println("Length of this SLL is"+ sll.length()); 
+        sll.insertLast(22);
+        sll.display();
+    //    System.out.println("Length of this SLL is"+ sll.length()); 
     }
 
 
