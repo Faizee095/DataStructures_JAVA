b566c62bda52fe757612e30b6d1900c9314e86fc
Faizee095 <faiyazsiddique9@gmail.com>
2022-03-10 18:21:42 +0530

    Insert at a given poisition

--- Singly Link List/SinglyLinkedlList.java
+++ Singly Link List/SinglyLinkedlList.java
@@ -38,6 +38,27 @@ public class SinglyLinkedlList{
         head=n1;
     }
 
+    public void insertAtPoisition(int poisition,int value){
+        ListNode node =new ListNode(value);
+        if(poisition ==1){
+            node.next=head;
+            head =node;
+        }
+        else{
+            ListNode previous =head;
+            int count =1;
+            while(count <poisition -1){
+                previous=previous.next;
+                count++;
+            }
+            ListNode current=previous.next;
+            previous.next=node;
+             node.next=current;
+        }
+
+    }
+
+
     public void insertLast(int value){
         ListNode n1=new ListNode(value);
         if(head==null){
@@ -55,22 +76,30 @@ public class SinglyLinkedlList{
 
 
     public static void main(String[] args) {
-        SinglyLinkedlList sll=new SinglyLinkedlList();
-        sll.head=new ListNode(10);
-         ListNode Second=new ListNode(20);
+         SinglyLinkedlList sll=new SinglyLinkedlList();
+        // sll.head=new ListNode(10);
+        //  ListNode Second=new ListNode(20);
         // ListNode Third=new ListNode(30);
-        // ListNode Fourth=new ListNode(40);
+        // ListNode Fourth=new ListNode(40); 
 
 
-         sll.head.next=Second;
+         //sll.head.next=Second;
         // Second.next=Third;
         // Third.next=Fourth;
 
 
 
-        sll.insertLast(22);
-        sll.display();
-    //    System.out.println("Length of this SLL is"+ sll.length()); 
+        // sll.insertLast(12);
+        // sll.insertLast(22);
+        // sll.insertLast(32);
+        // sll.insertFirst(2);
+       
+        //  System.out.println("Length of this SLL is"+ sll.length()); 
+
+        sll.insertAtPoisition(1,7);
+        sll.insertAtPoisition(1,8);
+        sll.insertAtPoisition(3,9);
+         sll.display();
     }
 
 
